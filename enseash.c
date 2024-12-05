93a62e6a885598d63fa9a651636199ae8f8bfc70
Anasta <rares-ioan.anastasiu@ensea.fr>
2024-12-05 11:25:47 +0100

    Added support for pipe redirection with |

--- enseash.c
+++ enseash.c
@@ -3,9 +3,9 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <fcntl.h>
 #include <stdlib.h>
 #include <time.h>
+#include <fcntl.h>
 
 #define MAX_ARGS 100  // Maximum number of arguments
 
@@ -45,27 +45,94 @@ int main() {
             break;
         }
 
+        // Check for pipe redirection
+        char *pipe_pos = strchr(input, '|');
+        if (pipe_pos != NULL) {
+            // Split the input into two commands
+            *pipe_pos = '\0';  // Split the string
+            char *cmd1 = input;  // First command
+            char *cmd2 = pipe_pos + 1;  // Second command
+
+            // Parse the commands into arguments
+            char *args1[MAX_ARGS] = {NULL}, *args2[MAX_ARGS] = {NULL};
+            int arg_count1 = 0, arg_count2 = 0;
+
+            char *token = strtok(cmd1, " ");
+            while (token != NULL && arg_count1 < MAX_ARGS - 1) {
+                args1[arg_count1++] = token;
+                token = strtok(NULL, " ");
+            }
+            args1[arg_count1] = NULL;
+
+            token = strtok(cmd2, " ");
+            while (token != NULL && arg_count2 < MAX_ARGS - 1) {
+                args2[arg_count2++] = token;
+                token = strtok(NULL, " ");
+            }
+            args2[arg_count2] = NULL;
+
+            // Create a pipe
+            int pipe_fd[2];
+            if (pipe(pipe_fd) == -1) {
+                perror("pipe");
+                continue;
+            }
+
+            // Fork for the first command
+            pid_t pid1 = fork();
+            if (pid1 == -1) {
+                perror("fork");
+                continue;
+            }
+
+            if (pid1 == 0) {
+                // Child process 1: Write to the pipe
+                close(pipe_fd[0]);  // Close unused read end
+                dup2(pipe_fd[1], STDOUT_FILENO);  // Redirect stdout to pipe
+                close(pipe_fd[1]);  // Close write end
+
+                execvp(args1[0], args1);
+                perror("execvp");
+                _exit(1);
+            }
+
+            // Fork for the second command
+            pid_t pid2 = fork();
+            if (pid2 == -1) {
+                perror("fork");
+                continue;
+            }
+
+            if (pid2 == 0) {
+                // Child process 2: Read from the pipe
+                close(pipe_fd[1]);  // Close unused write end
+                dup2(pipe_fd[0], STDIN_FILENO);  // Redirect stdin to pipe
+                close(pipe_fd[0]);  // Close read end
+
+                execvp(args2[0], args2);
+                perror("execvp");
+                _exit(1);
+            }
+
+            // Parent process: Close pipe ends and wait for both children
+            close(pipe_fd[0]);
+            close(pipe_fd[1]);
+            waitpid(pid1, &last_status, 0);
+            waitpid(pid2, &last_status, 0);
+
+            continue;
+        }
+
         // Parse the input into command and arguments
         char *args[MAX_ARGS] = {NULL};
-        char *file_in = NULL, *file_out = NULL;  // For redirection
-        int arg_count = 0, redirect_in = 0, redirect_out = 0;
+        int arg_count = 0;
 
         char *token = strtok(input, " ");
-        while (token != NULL) {
-            if (strcmp(token, "<") == 0) {
-                redirect_in = 1;
-                token = strtok(NULL, " ");
-                if (token) file_in = token;
-            } else if (strcmp(token, ">") == 0) {
-                redirect_out = 1;
-                token = strtok(NULL, " ");
-                if (token) file_out = token;
-            } else {
-                args[arg_count++] = token;
-            }
+        while (token != NULL && arg_count < MAX_ARGS - 1) {
+            args[arg_count++] = token;
             token = strtok(NULL, " ");
         }
-        args[arg_count] = NULL;  // Null-terminate the argument list
+        args[arg_count] = NULL;
 
         // Fork a new process to execute the command
         pid_t pid = fork();
@@ -75,27 +142,7 @@ int main() {
         }
 
         if (pid == 0) {
-            // Child process: Set up redirections if specified
-            if (redirect_in && file_in) {
-                int fd_in = open(file_in, O_RDONLY);
-                if (fd_in == -1) {
-                    perror("open");
-                    _exit(1);
-                }
-                dup2(fd_in, STDIN_FILENO);  // Redirect stdin
-                close(fd_in);
-            }
-            if (redirect_out && file_out) {
-                int fd_out = open(file_out, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-                if (fd_out == -1) {
-                    perror("open");
-                    _exit(1);
-                }
-                dup2(fd_out, STDOUT_FILENO);  // Redirect stdout
-                close(fd_out);
-            }
-
-            // Execute the command with arguments
+            // Child process: Execute the command with arguments
             execvp(args[0], args);
             // If execvp fails, print an error and exit the child process
             perror("execvp");
