72158571a43825131f3ac67264b35c6bcb8fcc87
Anasta <rares-ioan.anastasiu@ensea.fr>
2024-12-09 17:06:25 +0100

    Implemented RFC-compliant RRQ construction in gettftp and Added tests for downloading various files from TFTP server

--- TP2/gettftp.c
+++ TP2/gettftp.c
@@ -7,7 +7,6 @@
 
 #define TFTP_PORT 69
 #define BUFFER_SIZE 516  // TFTP packets are max 516 bytes
-#define DATA_SIZE 512    // Max data size for a TFTP data packet
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
@@ -57,12 +56,17 @@ int main(int argc, char *argv[]) {
     char buffer[BUFFER_SIZE];
     int rrq_len = 0;
 
-    buffer[rrq_len++] = 0x00;  // Opcode (1 for RRQ)
+    // Add Opcode (1 for RRQ)
+    buffer[rrq_len++] = 0x00;
     buffer[rrq_len++] = 0x01;
-    strcpy(&buffer[rrq_len], filename);  // Filename
-    rrq_len += strlen(filename) + 1;    // +1 for null terminator
-    strcpy(&buffer[rrq_len], "octet");  // Mode (binary transfer)
-    rrq_len += strlen("octet") + 1;     // +1 for null terminator
+
+    // Add Filename (null-terminated)
+    strcpy(&buffer[rrq_len], filename);
+    rrq_len += strlen(filename) + 1;  // +1 for null character
+
+    // Add Transfer Mode (null-terminated)
+    strcpy(&buffer[rrq_len], "octet");
+    rrq_len += strlen("octet") + 1;  // +1 for null character
 
     // Send the RRQ packet to the server
     if (send(sockfd, buffer, rrq_len, 0) < 0) {
@@ -73,59 +77,6 @@ int main(int argc, char *argv[]) {
 
     printf("RRQ sent for file: %s\n", filename);
 
-    // Open a file to write the received data
-    FILE *file = fopen(filename, "wb");
-    if (!file) {
-        perror("Failed to open file for writing");
-        close(sockfd);
-        return EXIT_FAILURE;
-    }
-
-    int block_num = 1;  // Expected block number
-    while (1) {
-        // Receive a DATA packet
-        int recv_len = recv(sockfd, buffer, BUFFER_SIZE, 0);
-        if (recv_len < 0) {
-            perror("Failed to receive DATA packet");
-            break;
-        }
-
-        // Validate the DATA packet
-        if (buffer[1] != 0x03) {  // Opcode (3 for DATA)
-            fprintf(stderr, "Unexpected packet received\n");
-            break;
-        }
-
-        int received_block = (buffer[2] << 8) | buffer[3];
-        if (received_block != block_num) {
-            fprintf(stderr, "Unexpected block number: %d\n", received_block);
-            break;
-        }
-
-        printf("Received DATA packet, block: %d, size: %d bytes\n", block_num, recv_len - 4);
-
-        // Write data to the file
-        fwrite(buffer + 4, 1, recv_len - 4, file);
-
-        // Send an ACK for the received DATA packet
-        char ack[4] = {0x00, 0x04, buffer[2], buffer[3]};
-        if (send(sockfd, ack, sizeof(ack), 0) < 0) {
-            perror("Failed to send ACK");
-            break;
-        }
-
-        printf("ACK sent for block: %d\n", block_num);
-
-        block_num++;
-
-        // If the last packet was smaller than 512 bytes, we're done
-        if (recv_len < BUFFER_SIZE) {
-            printf("File transfer complete.\n");
-            break;
-        }
-    }
-
-    fclose(file);
     close(sockfd);
     return EXIT_SUCCESS;
 }
